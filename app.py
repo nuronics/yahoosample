2ac756a623e12ffb2ec99c10282e9d4e8c08c682
nuronics <42369707+nuronics@users.noreply.github.com>
2018-08-25 11:57:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,8 @@
 #!/usr/bin/python
-from __future__ import print_function
-from future.standard_library import install_aliases
-install_aliases()
-
-from urllib.parse import urlparse, urlencode
-from urllib.request import urlopen, Request
-from urllib.error import HTTPError
-
+import urllib
 import json
 import os
-
+ 
 from flask import Flask
 from flask import request
 from flask import make_response
