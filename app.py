f31fec3736e83508e9480c59eb842992d8394fd4
nuronics <42369707+nuronics@users.noreply.github.com>
2018-08-25 12:03:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -94,7 +94,6 @@ def makeWebhookResult(data):
     }
 
 if __name__ == '__main__':
-
     port = int(os.getenv('PORT', 5000))
     print("Starting app on port %d" % port)
     app.run(debug=Falsse, port=port, host='0.0.0.0')
