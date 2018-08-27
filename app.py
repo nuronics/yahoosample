4b864699f7e6520fd5724c7a7e8f91065a4e0fff
nuronics <42369707+nuronics@users.noreply.github.com>
2018-08-27 16:58:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def processRequest(req):
         yql_query = makeYqlQuery(req)
         if yql_query is None:
             return {}
-        yql_url = baseurl + urllib.urlencode({'q': yql_query}) + "&format=json"
+        yql_url = baseurl + urllib.parse.urlencode({'q': yql_query}) + "&format=json"
         result = urlopen(yql_url).read()
         data = json.loads(result)
         res = makeWebhookResult(data)
