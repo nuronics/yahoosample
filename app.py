725f52bea9cb8e2e287b311aa612c407d4ed2981
nuronics <42369707+nuronics@users.noreply.github.com>
2018-08-27 16:39:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def webhook():
 
     print("Request:")
     print(json.dumps(req, indent=4))
-
+    req=json.dumps(req, indent=4)
     res = processRequest(req)
 
     res = json.dumps(res, indent=4)
